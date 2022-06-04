6ba18ce6b0e2a7fb9ae2cd8c3c1220c911b35d2b
Jo Emil Holen <jo-emil@outlook.com>
2022-06-04 16:02:31 +0200

    add Jøkul🚀🤘

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,22 +7,39 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import coreStyle from "@fremtind/jkl-core/core.min.css";
+import rootStyle from "./styles/root.css";
+
+const jklStyles: string[] = [coreStyle];
+
+export const links: LinksFunction = () => [
+  {
+    href: rootStyle,
+    rel: "stylesheet",
+  },
+  ...jklStyles.map((style) => ({
+    href: style,
+    rel: "stylesheet",
+  })),
+];
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Premiehåndtering - The Gathering",
   viewport: "width=device-width,initial-scale=1",
 });
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="no">
       <head>
         <Meta />
         <Links />
       </head>
-      <body>
-        <Outlet />
+      <body className="jkl" data-theme="light">
+        <main>
+          <Outlet />
+        </main>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,34 @@
+import { Link } from "@fremtind/jkl-core";
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+    <>
+      <h1 className="jkl-heading-1">Welcome to Remix</h1>
       <ul>
         <li>
-          <a
+          <Link
             target="_blank"
             href="https://remix.run/tutorials/blog"
             rel="noreferrer"
           >
             15m Quickstart Blog Tutorial
-          </a>
+          </Link>
         </li>
         <li>
-          <a
+          <Link
             target="_blank"
             href="https://remix.run/tutorials/jokes"
             rel="noreferrer"
           >
             Deep Dive Jokes App Tutorial
-          </a>
+          </Link>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+          <Link target="_blank" href="https://remix.run/docs" rel="noreferrer">
             Remix Docs
-          </a>
+          </Link>
         </li>
       </ul>
-    </div>
+    </>
   );
 }
